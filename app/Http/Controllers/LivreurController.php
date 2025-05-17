473cfa8690dfbbb29cae24270f16196e0ba5ccd3
mohammedennaim <mohammedennaim99@gmail.com>
2025-05-17 21:32:58 +0100

    Refactor LivreurController for improved readability and maintainability

--- app/Http/Controllers/LivreurController.php
+++ app/Http/Controllers/LivreurController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Livraison;
 use App\Models\Livreur;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
